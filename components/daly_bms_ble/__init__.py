9051c028bd4e147c3bf102abbc0d019d99a56407
Bayyo1337 <115353825+Bayyo1337@users.noreply.github.com>
2026-01-16 13:17:30 +0100

    Replace the entire content of __init__.py with the BLE client configuration

--- components/daly_bms_ble/__init__.py
+++ components/daly_bms_ble/__init__.py
@@ -1,1 +1,17 @@
-AUTO_LOAD = ["binary_sensor", "button", "number", "sensor", "text_sensor", "switch"]
+# Import necessary ESPHome components
+import esphome.config_schema as sche
+import esphome.codegen as cg
+
+# Define constants for DALY BMS BLE
+CONF_DALY_BMS_BLE_ID = "daly_bms_ble"  
+DALY_BMS_BLE_COMPONENT_SCHEMA = sche.Schema({
+    cv.Required(CONF_DALY_BMS_BLE_ID): cv.declare_id(),  
+})
+CONFIG_SCHEMA = DALY_BMS_BLE_COMPONENT_SCHEMA.extend(cv.COMPONENT_SCHEMA)
+
+# Function to generate code for the BLE client component
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_DALY_BMS_BLE_ID])
+    # Here you would add the specific setup for the BLE client implementation
+    # e.g., connecting to the BLE device, setting up characteristics, etc.
+    cg.add(var)  
