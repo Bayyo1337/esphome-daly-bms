02867171759570ca713ac6872df0fd6f232568d3
Bayyo1337 <115353825+Bayyo1337@users.noreply.github.com>
2026-01-16 13:19:24 +0100

    Refactor DALY BMS BLE component initialization

--- components/daly_bms_ble/__init__.py
+++ components/daly_bms_ble/__init__.py
@@ -1,17 +1,42 @@
-# Import necessary ESPHome components
-import esphome.config_schema as sche
 import esphome.codegen as cg
+from esphome.components import ble_client
+import esphome.config_validation as cv
+from esphome.const import CONF_ID, CONF_PASSWORD
 
-# Define constants for DALY BMS BLE
-CONF_DALY_BMS_BLE_ID = "daly_bms_ble"  
-DALY_BMS_BLE_COMPONENT_SCHEMA = sche.Schema({
-    cv.Required(CONF_DALY_BMS_BLE_ID): cv.declare_id(),  
-})
-CONFIG_SCHEMA = DALY_BMS_BLE_COMPONENT_SCHEMA.extend(cv.COMPONENT_SCHEMA)
+CODEOWNERS = ["@syssi"]
 
-# Function to generate code for the BLE client component
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_DALY_BMS_BLE_ID])
-    # Here you would add the specific setup for the BLE client implementation
-    # e.g., connecting to the BLE device, setting up characteristics, etc.
-    cg.add(var)  
+AUTO_LOAD = ["binary_sensor", "button", "number", "sensor", "text_sensor", "switch"]
+MULTI_CONF = True
+
+CONF_DALY_BMS_BLE_ID = "daly_bms_ble_id"
+
+daly_bms_ble_ns = cg.esphome_ns.namespace("daly_bms_ble")
+DalyBmsBle = daly_bms_ble_ns.class_(
+    "DalyBmsBle", ble_client.BLEClientNode, cg. PollingComponent
+)
+
+DALY_BMS_BLE_COMPONENT_SCHEMA = cv. Schema(
+    {
+        cv.GenerateID(CONF_DALY_BMS_BLE_ID): cv.use_id(DalyBmsBle),
+    }
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(DalyBmsBle),
+            cv.Optional(CONF_PASSWORD, default=12345678): cv.uint32_t,
+        }
+    )
+    .extend(cv.polling_component_schema("10s"))
+    .extend(ble_client.BLE_CLIENT_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await ble_client.register_ble_node(var, config)
+
+    if CONF_PASSWORD in config: 
+        cg.add(var.set_password(config[CONF_PASSWORD]))
